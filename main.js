e9015cf00186ff686a91f29b21322e5286a5371a
Wammy1995 <linqi19951102@126.com>
2021-12-05 12:00:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log(teclas);
 var cuadrito =document.getElementById("garabato");
 var papel = cuadrito.getContext("2d");
 document.addEventListener("keydown",dibujarTeclado);
+document.addEventListener("keyup",songshou);
 // document.addEventListener("click",tecladito);
 var teclas =
 {
@@ -15,6 +16,10 @@ var teclas =
 var color = "red"
 var y=150
 var x= 150
+var m_left = false;   
+var m_right = false;
+var m_down = false;
+var m_up = false;
 // function tecladito(evento)
 // {
 //    if (evento.click==true){
@@ -28,28 +33,41 @@ function dibujarTeclado(evento)
    switch(evento.keyCode)
    {
       case teclas.UP:
-         dibujarLinea("color",x,y,x,y-3,papel);
-         y=y-3;
+         m_up = true;
       break
       case teclas.DOWN:
-         dibujarLinea("color",x,y,x,y+3,papel);
-         y=y+3;
+         m_down = true;
       break
       case teclas.LEFT:
-         dibujarLinea("color",x,y,x-4,y,papel);
-         x=x-4;
+         m_left = true;
       break
       case teclas.RIGHT:
-         dibujarLinea("color",x,y,x+4,y,papel);
-         x=x+4;
-      break
-      case teclas.RIGHT:
-         dibujarLinea("color",x,y,x+4,y,papel);
-         x=x+4;
+         m_right = true;
       break
    }
    
 }
+
+function shousong(evento)
+{
+   switch(evento.keyCode)
+   {
+      case teclas.UP:
+         m_up = false;
+      break
+      case teclas.DOWN:
+         m_down = false;
+      break
+      case teclas.LEFT:
+         m_left = false;
+      break
+      case teclas.RIGHT:
+         m_right = false;
+      break
+   }
+   
+}
+
 
 dibujarLinea(color,149,149,151,151,papel)
 
@@ -63,4 +81,22 @@ function dibujarLinea(color,x_inicial,y_inicial,x_final,y_final,lienzo)
         lienzo.closePath();
     }
 
+setInterval(function(){   
+    if(a_left){
+      dibujarLinea("color",x,y,x,y-5,papel);
+      y=y-5;
+    }
+    if(a_right){
+      dibujarLinea("color",x,y,x,y+5,papel);
+      y=y+5;
+    }
+    if(b_left){
+      dibujarLinea("color",x,y,x-5,y,papel);
+      x=x-5;
+    }
+    if(b_right){
+      dibujarLinea("color",x,y,x+5,y,papel);
+      x=x+5;
+    }
+},5)
 
