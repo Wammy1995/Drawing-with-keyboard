40d67180b5e4717466df5d123325ba0d7f1df35d
Wammy1995 <linqi19951102@126.com>
2021-12-07 12:52:24 +0800

    32

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-var canvas = document.getElementById("test");   
+// var canvas = document.getElementById("test1");   
+//     var cxt = canvas.getContext("2d");   
+//     cxt.beginPath();   
+//     //设置是个顶点的坐标，根据顶点制定路径   
+//     for (var i = 0; i < 5; i++) {   
+//         cxt.lineTo(Math.cos((18+i*72)/180*Math.PI)*200+200,   
+//                         -Math.sin((18+i*72)/180*Math.PI)*200+200);   
+//         cxt.lineTo(Math.cos((54+i*72)/180*Math.PI)*80+200,   
+//                         -Math.sin((54+i*72)/180*Math.PI)*80+200);   
+//     }   
+//     cxt.closePath();   
+//     //设置边框样式以及填充颜色   
+//     cxt.lineWidth="3";
+//     cxt.fillStyle = "#F6F152";
+//     cxt.strokeStyle = "#F5270B"; 
+//     cxt.fill();
+//     cxt.stroke();
+// 以上是五角星
+
+var canvas = document.getElementById("test2");   
     var cxt = canvas.getContext("2d");   
     cxt.beginPath();   
     //设置是个顶点的坐标，根据顶点制定路径   
-    for (var i = 0; i < 5; i++) {   
-        cxt.lineTo(Math.cos((18+i*72)/180*Math.PI)*200+200,   
-                        -Math.sin((18+i*72)/180*Math.PI)*200+200);   
-        cxt.lineTo(Math.cos((54+i*72)/180*Math.PI)*80+200,   
-                        -Math.sin((54+i*72)/180*Math.PI)*80+200);   
-    }   
+    cxt.lineTo(30,30);
+    cxt.lineTo(130,30);
+    cxt.lineTo(160,70);
+    cxt.lineTo(60,70);
     cxt.closePath();   
     //设置边框样式以及填充颜色   
     cxt.lineWidth="3";
@@ -16,8 +33,6 @@ var canvas = document.getElementById("test");
     cxt.fill();
     cxt.stroke();
 // 以上是五角星
-
-
 
 
 var cuadrito =document.getElementById("garabato");
@@ -34,14 +49,12 @@ var teclas =
 };
 
 var color = "black"
-// var y = 100
-// var x = 100
-var y = -Math.sin(18/180*Math.PI)*200+200
-var x = Math.cos(18/180*Math.PI)*200+200
-var xspeed = 6
-var yspeed = 6
-var newy = 100
-var newx = 100
+var y = 30
+var x = 32
+var xspeed = 1
+var yspeed = 1
+var newy = y
+var newx = x
 var m_left = false;   
 var m_right = false;
 var m_down = false;
@@ -95,9 +108,7 @@ function songshou(evento)
 }
 
 
-// dibujarLinea('red',99,99,100,99,papel)
-dibujarLinea('red',Math.cos(18/180*Math.PI)*200+200,-Math.sin(18/180*Math.PI)*200+200,
-   Math.cos(18/180*Math.PI)*200+200,-Math.sin(18/180*Math.PI)*200+200,papel)
+dibujarLinea('blue',30,30,32,30,papel)
 
 function dibujarLinea(color,x_inicial,y_inicial,x_final,y_final,lienzo)
     {
@@ -110,7 +121,7 @@ function dibujarLinea(color,x_inicial,y_inicial,x_final,y_final,lienzo)
         lienzo.closePath();
     }
 
-setInterval(function(){   
+setInterval(function(){
     if(m_down){
       newy+=yspeed;
     }
@@ -129,4 +140,4 @@ setInterval(function(){
 },50)
 
 // papel.fill() 最后闭合图形
-// papel.getImageData(0,0,400,400).data 获取数据点信息
+// papel.getImageData(0,0,400,400).data 获取数据点信息 4x-1
