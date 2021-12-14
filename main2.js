4e88ef23b781bdf7a2e557896ef5431b4e146af0
Wammy1995 <linqi19951102@126.com>
2021-12-14 12:21:43 +0800

    2

--- main2.js
+++ main2.js
@@ -203,7 +203,7 @@ var ick = setInterval(function(){
       newx+=xspeed;
     }
     var ds = Math.sqrt(Math.pow((newx-x_in),2)+Math.pow((newy-y_in),2))
-    if (ds<=xspeed){
+    if (ds<xspeed){
       tts = false; 
     }else{
       tts = true;
